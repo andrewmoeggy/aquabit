5fa6e23e475fd93390b16d2b7eb56ac267785190
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-03-18 14:03:52 -0400

    smaller photo links added

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -4,52 +4,61 @@ const SHOP_DATA = [
     title: 'Laptops',
     routeName: 'laptops',
     items: [
+
+
+
+
+
+
+
+
+
       {
         id: 1,
         name: 'HP 250',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/n8mZM4T/hands-woman-legs-laptop.jpg',
         price: 25
       },
       {
         id: 2,
         name: 'Mac 250',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/1rtLnCD/pexels-photo.jpg',
         price: 18
       },
       {
         id: 3,
         name: 'Lenovo R9sV',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/h2M67Xt/pexels-photo-461077.jpg',
         price: 35
       },
       {
         id: 4,
         name: 'Toshiba U89',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/wywpZbM/pexels-photo-374074.jpg',
         price: 25
       },
       {
         id: 5,
         name: 'HP 98VV',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/NCmjVSM/pexels-photo-374016.jpg',
         price: 18
       },
       {
         id: 6,
         name: 'The ENIAC',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/5khFdHj/pexels-photo-1006293.jpg',
         price: 14
       },
       {
         id: 7,
         name: 'MacBook Pro',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/gZQG8Fd/pexels-photo-1229861.jpg',
         price: 18
       },
       {
         id: 8,
         name: 'Lenovo 12',
-        imageUrl: 'https://loremflickr.com/320/240/laptop',
+        imageUrl: 'https://i.ibb.co/cYPb9Hz/pexels-photo-2047905.jpg',
         price: 14
       }
     ]
@@ -58,141 +67,138 @@ const SHOP_DATA = [
     id: 2,
     title: 'Desktops',
     routeName: 'desktops',
-    items: [
-      {
-        id: 1,
-        name: 'Dell Opti',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 220
-      },
-      {
-        id: 2,
-        name: 'Acer Aspire',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 280
-      },
-      {
-        id: 3,
-        name: 'Hp EliteDesk',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 110
-      },
-      {
-        id: 4,
-        name: 'Acer Desktop',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 160
-      },
-      {
-        id: 5,
-        name: 'Apple iMac',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 160
-      },
-      {
-        id: 6,
-        name: 'Asus 940',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 160
-      },
-      {
-        id: 7,
-        name: 'Toshiba Desktop',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 190
-      },
-      {
-        id: 8,
-        name: 'UltraFax Fax 9000',
-        imageUrl: 'https://loremflickr.com/320/240/desktop',
-        price: 200
-      }
+    items: [{
+      id: 1,
+      name: 'Dell Opti',
+      imageUrl: 'https://i.ibb.co/L99b9nH/apple-imac-ipad-workplace-38568.jpg',
+      price: 220
+    },
+    {
+      id: 2,
+      name: 'Acer Aspire',
+      imageUrl: 'https://i.ibb.co/60D3ryM/pexels-photo-60740.jpg',
+      price: 280
+    },
+    {
+      id: 3,
+      name: 'Hp EliteDesk',
+      imageUrl: 'https://i.ibb.co/j5GwX56/pexels-photo-115655.jpg',
+      price: 110
+    },
+    {
+      id: 4,
+      name: 'Acer Desktop',
+      imageUrl: 'https://i.ibb.co/Cv0TVNL/pexels-photo-322338.jpg',
+      price: 160
+    },
+    {
+      id: 5,
+      name: 'Apple iMac',
+      imageUrl: 'https://i.ibb.co/FJRWz0m/pexels-photo-450035.jpg',
+      price: 160
+    },
+    {
+      id: 6,
+      name: 'Asus 940',
+      imageUrl: 'https://i.ibb.co/kggGy4t/pexels-photo-572056.jpg',
+      price: 160
+    },
+    {
+      id: 7,
+      name: 'Toshiba Desktop',
+      imageUrl: 'https://i.ibb.co/rk7rWm7/pexels-photo-777001.jpg',
+      price: 190
+    },
+    {
+      id: 8,
+      name: 'UltraFax Fax 9000',
+      imageUrl: 'https://i.ibb.co/T29T692/pexels-photo-1714202.jpg',
+      price: 200
+    }
     ]
   },
   {
     id: 3,
     title: 'Watches',
     routeName: 'watches',
-    items: [
-      {
-        id: 1,
-        name: 'iWatch',
-        imageUrl: 'https://loremflickr.com/320/240/watch',
-        price: 125
-      },
-      {
-        id: 2,
-        name: 'Rolex 12',
-        imageUrl: 'https://loremflickr.com/320/240/watch',
-        price: 90
-      },
-      {
-        id: 3,
-        name: 'Invicta',
-        imageUrl: 'https://loremflickr.com/320/240/watch',
-        price: 90
-      },
-      {
-        id: 4,
-        name: 'Citizen Eco',
-        imageUrl: 'https://loremflickr.com/320/240/watch',
-        price: 165
-      },
-      {
-        id: 5,
-        name: 'GV2 Gevril',
-        imageUrl: 'https://loremflickr.com/320/240/watch',
-        price: 185
-      }
+    items: [{
+      id: 1,
+      name: 'iWatch',
+      imageUrl: 'https://i.ibb.co/WPb8DSX/pexels-photo-125779.jpg',
+      price: 125
+    },
+    {
+      id: 2,
+      name: 'Rolex 12',
+      imageUrl: 'https://i.ibb.co/dkDFv3d/pexels-photo-190819.jpg',
+      price: 90
+    },
+    {
+      id: 3,
+      name: 'Invicta',
+      imageUrl: 'https://i.ibb.co/jTyv32q/pexels-photo-236915.jpg',
+      price: 90
+    },
+    {
+      id: 4,
+      name: 'Citizen Eco',
+      imageUrl: 'https://i.ibb.co/qC8P3V5/pexels-photo-592815.jpg',
+      price: 165
+    },
+    {
+      id: 5,
+      name: 'GV2 Gevril',
+      imageUrl: 'https://i.ibb.co/dgYh3q7/pexels-photo-1034063.jpg',
+      price: 185
+    }
     ]
   },
   {
     id: 4,
     title: 'Televisons',
     routeName: 'televison',
-    items: [
-      {
-        id: 1,
-        name: 'Samsung Q70R',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 25
-      },
-      {
-        id: 2,
-        name: 'LG OLED',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 20
-      },
-      {
-        id: 3,
-        name: 'VIZIO 241',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 80
-      },
-      {
-        id: 4,
-        name: 'Sony IO',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 80
-      },
-      {
-        id: 5,
-        name: 'Apple TV',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 45
-      },
-      {
-        id: 6,
-        name: 'Samsung 94Q',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 135
-      },
-      {
-        id: 7,
-        name: 'Philips R29',
-        imageUrl: 'https://loremflickr.com/320/240/televison',
-        price: 20
-      }
+    items: [{
+      id: 1,
+      name: 'Samsung Q70R',
+      imageUrl: 'https://i.ibb.co/9bR7w3z/pexels-photo-333984.jpg',
+      price: 25
+    },
+    {
+      id: 2,
+      name: 'LG OLED',
+      imageUrl: 'https://i.ibb.co/QNP0hPK/pexels-photo-704555.jpg',
+      price: 20
+    },
+    {
+      id: 3,
+      name: 'VIZIO 241',
+      imageUrl: 'https://i.ibb.co/X33MFPK/pexels-photo-1201996.jpg',
+      price: 80
+    },
+    {
+      id: 4,
+      name: 'Sony IO',
+      imageUrl: 'https://i.ibb.co/VwVpcPd/pexels-photo-2251206.jpg',
+      price: 80
+    },
+    {
+      id: 5,
+      name: 'Apple TV',
+      imageUrl: 'https://i.ibb.co/jz80L2y/pexels-photo-2728255.jpg',
+      price: 45
+    },
+    {
+      id: 6,
+      name: 'Samsung 94Q',
+      imageUrl: 'https://i.ibb.co/BCtcJLv/pexels-photo-3151392.jpg',
+      price: 135
+    },
+    {
+      id: 7,
+      name: 'Philips R29',
+      imageUrl: 'https://i.ibb.co/3Wscy3h/pexels-photo-3356608.jpg',
+      price: 20
+    }
     ]
   },
   {
@@ -203,37 +209,37 @@ const SHOP_DATA = [
       {
         id: 1,
         name: 'iPhone A',
-        imageUrl: 'https://loremflickr.com/320/240/phone',
+        imageUrl: 'https://i.ibb.co/p1NM8RX/pexels-photo-336948.jpg',
         price: 325
       },
       {
         id: 2,
         name: 'iPhone B',
-        imageUrl: 'https://loremflickr.com/320/240/phone',
+        imageUrl: 'https://i.ibb.co/mh1DHk0/pexels-photo-607812.jpg',
         price: 20
       },
       {
         id: 3,
         name: 'iPhone C',
-        imageUrl: 'https://loremflickr.com/320/240/phone',
+        imageUrl: 'https://i.ibb.co/m4FjSvF/pexels-photo-1194760.jpg',
         price: 25
       },
       {
         id: 4,
         name: 'iPhone D',
-        imageUrl: 'https://loremflickr.com/320/240/phone',
+        imageUrl: 'https://i.ibb.co/g455mH4/pexels-photo-887751.jpg',
         price: 25
       },
       {
         id: 5,
         name: 'iPhone E',
-        imageUrl: 'https://loremflickr.com/320/240/phone',
+        imageUrl: 'https://i.ibb.co/6NY9bCZ/pexels-photo-1092644.jpg',
         price: 40
       },
       {
         id: 6,
         name: 'iPhone F',
-        imageUrl: 'https://loremflickr.com/320/240/phone',
+        imageUrl: 'https://i.ibb.co/RTZDNmM/pexels-photo-699122.jpg',
         price: 25
       }
     ]
