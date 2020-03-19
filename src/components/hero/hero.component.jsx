c9b458dcc4101883f5bb64dd7e8b14569efb833a
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-03-19 12:21:00 -0400

    fixed minor issue

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './hero.styles.scss';
 
 import Button from '../button/button.component';
@@ -7,7 +8,9 @@ const Hero = ({ backgroundImg, heading, textColor }) => {
   return (
     <main className="hero-main" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.0)),url(${backgroundImg})` }}>
       <h1 className="hero-heading" style={{ color: `${textColor}` }}>{heading}</h1>
-      <Button buttonText={'Browse our Products'} />
+      <Link to='/shop'>
+        <Button buttonText={'Browse our Products'} />
+      </Link>
     </main>
   );
 }
