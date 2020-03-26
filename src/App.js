0e776824942d023b14b53114257441b532c502ab
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-03-26 16:27:05 -0400

    header updated

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/shop' component={Shop} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <div className="header-container">
@@ -12,7 +13,11 @@ const Header = () => {
         <div className="header-links">
           <Link to='/shop' className='header-links--item'>SHOP</Link>
           <Link to='/shop' className='header-links--item'>ABOUT</Link>
-          <Link to='/shop' className='header-links--item'>CONTACT</Link>
+          {currentUser ?
+            <div className='header-links--item' onClick={() => auth.signOut()}>Sign Out</div>
+            :
+            <Link className='header-links--item' to='/signin'>Sign In</Link>
+          }
         </div>
       </div>
     </div>
