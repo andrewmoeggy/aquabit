6c95b445b4c1487d7e64aafe23b1f32dd203d852
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-03-26 12:20:33 -0400

    styling for signin page

--- src/components/signIn/signIn.component.jsx
+++ src/components/signIn/signIn.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './signIn.styles.scss';
 
 class SignIn extends Component {
   constructor(props) {
@@ -26,23 +27,26 @@ class SignIn extends Component {
     return (
       <div className="signIn">
         <form>
-          <label>Email</label>
+          <span className='signIn-header'>Sign in with email password</span>
+          <label className='signIn-form-label'>Email</label>
           <input
+            className='signIn-form-input'
             type="email"
             name="email"
             value={this.state.email}
             onChange={this.handleChange}
             required
           />
-          <label>Password</label>
+          <label className='signIn-form-label'>Password</label>
           <input
+            className='signIn-form-input'
             type="password"
             name="password"
             value={this.state.password}
             onChange={this.handleChange}
             required
           />
-          <input type='submit' value='Submit' />
+          <input type='submit' value='Submit' className='signIn-form-submit' />
         </form>
       </div>
     );
