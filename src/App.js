7443d32012d37a6418572839a05e09c29eb54e34
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-03-26 16:09:40 -0400

    set up unsubscribe from auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import { auth } from './firebase/firebase.utils';
 
 import Header from './components/header/header.component';
 import Home from './pages/home/homepage.component';
@@ -8,19 +9,39 @@ import Shop from './pages/shop/shop.component';
 import SignInSignUp from './pages/signIn-signUp/signIn-signUp.component';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/signin' component={SignInSignUp} />
-      </Switch>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: null
+    }
+  }
 
-    </div>
+  unsubscribeFromAuth = null
 
-  );
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
